68325325040fa4bbe792a3347ac89e5fed02c2fd
Dan Neumann <danrodneu@gmail.com>
2011-12-31 17:11:27 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Symposium::Application.config.secret_token = 'b95e923b48368d0b0d3a622158e9f1b6fb6633d431d442dd74e719a72b4aa8bbd427bfbfe50cb2497a318c151d6dba2871331438c6eafaa5d138bf01826e1eb3'
