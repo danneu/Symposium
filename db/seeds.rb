6dd41315b88a40a62c2e3a3220a3bdcbef1f10e6
Dan Neumann <danrodneu@gmail.com>
2012-01-02 01:04:19 -0600

    forum seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Forum.destroy_all
+
+Forum.create(name: "News and Announcements",
+             description: "Official news is posted here.")
+Forum.create(name: "Newbie Introductions",
+             description: "Are you new? Come say hello!")
+Forum.create(name: "Off-Topic Discussion",
+             description: "Talk about anything.")
+
