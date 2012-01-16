57d3698ce5c9468fb319139a6de2fa363d900b3e
Terence Ponce <terenceponce@gmail.com>
2012-01-16 10:41:26 +0800

    Fixed existing integration tests

--- spec/requests/user_signups_spec.rb
+++ spec/requests/user_signups_spec.rb
@@ -1,34 +1,36 @@
 require 'spec_helper'
 
 describe "UserSignups" do
-  it "logs in user after successfully signing up" do
+  it "should log in user after successfully signing up" do
     visit root_path
     click_link "Sign Up"
     fill_in "Username", :with => "sample"
+    fill_in "Email", :with => "test@example.com"
     fill_in "Password", :with => "something"
     fill_in "Password confirmation", :with => "something"
     click_button "Sign Up"
-    page.should have_content("Successfully")
+    page.should have_content("Welcome! You have signed up successfully")
     page.should have_content("Log Out")
-    page.should have_content("sample")
+    page.should have_content("Welcome, sample")
   end
 
-  it "prevents the user from signing up when submitting a blank form" do
+  it "should return an error when submitting a blank form" do
     visit root_path
     click_link "Sign Up"
     click_button "Sign Up"
-    page.should have_content("You must correct the error")
+    page.should have_content("4 errors prohibited this user from being saved")
     page.should have_content("can't be blank")
   end
 
-  it "prevents the user from signing up when password and confirmation does not match" do
+  it "should return an error when password and password confirmation do not match" do
     visit root_path
     click_link "Sign Up"
     fill_in "Username", :with => "sample"
+    fill_in "Email", :with => "test@example.com"
     fill_in "Password", :with => "something"
     fill_in "Password", :with => "different"
     click_button "Sign Up"
-    page.should have_content("You must correct the error")
+    page.should have_content("2 errors prohibited this user from being saved")
     page.should have_content("doesn't match confirmation")
   end
 end
