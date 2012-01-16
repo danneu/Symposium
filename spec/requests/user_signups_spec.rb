3afedc5c30e20f3ef2e678dfeb29b65c298645f3
Terence Ponce <terenceponce@gmail.com>
2012-01-16 10:57:32 +0800

    Added more tests

--- spec/requests/user_signups_spec.rb
+++ spec/requests/user_signups_spec.rb
@@ -24,6 +24,40 @@ describe "UserSignups" do
     page.should have_content("Username can't be blank")
   end
 
+  it "should return an error when Username field is blank" do
+    visit root_path
+    click_link "Sign Up"
+    fill_in "Email", :with => "test@example.com"
+    fill_in "Password", :with => "bestpassword"
+    fill_in "Password confirmation", :with => "bestpassword"
+    click_button "Sign Up"
+    page.should have_content("1 error prohibited this user from being saved")
+    page.should have_content("Username can't be blank")
+  end
+
+  it "should return an error when Email field is blank" do
+    visit root_path
+    click_link "Sign Up"
+    fill_in "Username", :with => "sample"
+    fill_in "Password", :with => "bestpassword"
+    fill_in "Password confirmation", :with => "bestpassword"
+    click_button "Sign Up"
+    page.should have_content("1 error prohibited this user from being saved")
+    page.should have_content("Email can't be blank")
+  end
+
+  it "should return an error when password is below 6 characters" do
+    visit root_path
+    click_link "Sign Up"
+    fill_in "Username", :with => "sample"
+    fill_in "Email", :with => "test@example.com"
+    fill_in "Password", :with => "short"
+    fill_in "Password confirmation", :with => "short"
+    click_button "Sign Up"
+    page.should have_content("1 error prohibited this user from being saved")
+    page.should have_content("Password is too short (minimum is 6 characters)")
+  end
+
   it "should return an error when password and password confirmation do not match" do
     visit root_path
     click_link "Sign Up"
